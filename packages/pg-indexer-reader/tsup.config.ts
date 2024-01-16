cd64efe59010865e6954a8487415ecc99eb4b696
Nabs <43505437+NAB5@users.noreply.github.com>
2024-01-15 19:09:25 -0500

    fix: update build command to include all bind

--- packages/pg-indexer-reader/tsup.config.ts
+++ packages/pg-indexer-reader/tsup.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "bin/postgres-frontend.ts"],
+  entry: [
+    "src/index.ts",
+    "bin/postgres-frontend.ts",
+    "bin/clean-local-docker-indexer.ts",
+    "bin/local-docker-indexer.ts",
+  ],
   target: "esnext",
   format: ["esm"],
   dts: false,
