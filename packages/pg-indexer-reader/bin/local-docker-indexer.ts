6e34103b3e5aa9fe5c5870625f87b331f5815369
Nabs <43505437+NAB5@users.noreply.github.com>
2024-01-15 19:25:14 -0500

    fix: bin should run from package dir

--- packages/pg-indexer-reader/bin/local-docker-indexer.ts
+++ packages/pg-indexer-reader/bin/local-docker-indexer.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 import { execSync } from "child_process";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const packageDir = join(currentDir, "..");
 
 try {
+  process.chdir(packageDir);
   execSync("pnpm run start:local", { stdio: "inherit" });
 } catch (error) {
   console.error("Failed to start local docker indexer:", error);
