20af2ec0a8c8ca066d75a66e539fc625438a669b
Nabs <43505437+NAB5@users.noreply.github.com>
2024-06-07 13:42:16 -0400

    build: include types in package

--- packages/sync-stack/tsup.config.ts
+++ packages/sync-stack/tsup.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: {
+    index: "src/index.ts",
+    types: "src/types.ts",
+  },
   outDir: "dist",
   format: ["esm"],
   dts: true,
